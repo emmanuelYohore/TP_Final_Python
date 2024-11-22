cccd64947c7763dc4e8e24986fded12fbcc2fc30
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2024-11-22 14:49:12 +0100

    function clear

---
+++ others.py
@@ -0,0 +1,9 @@
+import os
+
+def detect_cycle(history, tour_actuel):
+    for i, grille_precedente in enumerate(history[:-1]):
+        if grille_precedente == history[-1]:
+            print(f"Cycle détecté! Début : Tour {i}, Cycle actuel : {tour_actuel - i} tours.")
+            break
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
