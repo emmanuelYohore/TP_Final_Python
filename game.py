5ac8c2a30ea710b3d61c556d05f0136420620f25
Godwin <godoblasse@gmail.com>
2024-11-22 13:28:02 +0100

    Initialisation de la grille - correction de nomenclature

--- game.py
+++ game.py
@@ -1,19 +1,19 @@
 import random
 
 class Game:
-    def __init__(self, grille=None, taille=10):
-        self.taille = taille
+    def __init__(self, grille=None, size=10):
+        self.size = size
         if grille :
             self.grille = grille
         else :
             self.grille = self.genererate_grille()
-        self.tour = 0
+        self.turn = 0
         self.history = []
 
     def genererate_grille(self):
-        return [[random.randint(0, 1) for _ in range(self.taille)] for _ in range(self.taille)]
+        return [[random.randint(0, 1) for _ in range(self.size)] for _ in range(self.size)]
 
     def show_grille(self):
         for ligne in self.grille:
             print(' '.join('⬜' if cellule else '⬛' for cellule in ligne))
-        print(f"Tour : {self.tour}")
+        print(f"Tour : {self.turn}")
